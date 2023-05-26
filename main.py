2e5212b138620f0aad3cd5c953f1a730043e10d6
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-26 18:05:12 +0530

    bind with the function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
-from tkinter import StringVar, Label, Tk, Entry
+from tkinter import END, StringVar, Label, Tk, Entry
 
 
 window = Tk()
-window.geometry("500x500")
+window.geometry("400x300")
 window.title("Monty Hall Simulator")
 
 same_choice = StringVar()
-switched_choice = StringVar
+switched_choice = StringVar()
 
 same_choice.set(0)
 switched_choice.set(0)
@@ -15,8 +15,10 @@ switched_choice.set(0)
 no_sample = Entry()
 
 Label1 = Label(window, text='Same Choice').place(x=80, y=8)
-label2 = Label(window, text='Switch Choice').place(x=80, y=40)
-label3 = Label(window, textvariable=same_choice, font=(15)).place(x=180, y=40)
+label2 = Label(window, text='Switched Choice').place(x=80, y=40)
+label3 = Label(window, textvariable=same_choice, font=(15)).place(x=180, y=8)
+label4 = Label(window, textvariable=switched_choice,
+               font=(15)).place(x=180, y=40)
 no_sample.place(x=100, y=70)
 
 
@@ -49,5 +51,7 @@ def simulate(event):
         else:
             print("That's will never happend.")
 
+
+no_sample.bind("<Return>", simulate)
 
 window.mainloop()
